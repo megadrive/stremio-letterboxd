cd843e56e0ca1ca167b535fc6cd5a991bd33067a
Andy S <a.simeon@gmail.com>
2024-01-16 11:42:04 +1100

    much faster

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -7,14 +7,14 @@ import { config } from "./consts";
 import { Watchlist_URL, does_letterboxd_user_exist } from "./util";
 const nameToImdb = promisify(name_to_imdb);
 
-type Movie = {
-  name: string;
-  type: "movie";
-  poster: string;
-  id: string;
-  description?: string;
-  releaseInfo?: string;
-};
+// type Movie = {
+//   name: string;
+//   type: "movie";
+//   poster: string;
+//   id: string;
+//   description?: string;
+//   releaseInfo?: string;
+// };
 
 type IFilm = {
   slug: string;
@@ -34,124 +34,56 @@ const is_old = (datetime: Date, howOld: number): boolean => {
   return rv;
 };
 
-async function get_tmdb_info(imdb_id: string) {
-  console.log(`[${imdb_id}]: Getting database cache`);
-  const cached = await prisma.movie.findUnique({
-    where: {
-      id: imdb_id,
-    },
-  });
-
-  if (cached && !is_old(cached.updated_at, config.cache_tmdb_stale_time * 24)) {
-    console.log(`[${imdb_id}]: Serving database cache`);
-    return cached;
-  }
-
-  console.log(`[${imdb_id}]: Fetching fresh data from TMDB`);
-  const { movie_results } = await (
-    await fetch(
-      `https://api.themoviedb.org/3/find/${imdb_id}?api_key=${process.env.TMDB_API_TOKEN}&external_source=imdb_id`
-    )
+async function get_meta_info(imdb_id: string) {
+  console.log(`[${imdb_id}]: Getting Cinemeta meta information`);
+  const meta = await (
+    await fetch(`https://v3-cinemeta.strem.io/meta/movie/${imdb_id}.json`)
   ).json();
 
-  if (!movie_results || !movie_results[0]) {
-    throw Error(`[${imdb_id}]: No results from TMBD.`);
-  }
-
-  const title = movie_results[0].title;
-  const overview = movie_results[0].overview;
-  const poster_path = movie_results[0].poster_path;
-  const release_date = String(movie_results[0].release_date?.split("-")[0]);
-
-  const data = {
-    id: imdb_id,
-    name: title,
-    poster: poster_path,
-    description: overview,
-    releaseInfo: release_date,
-  };
-
-  const upserted = await prisma.movie.upsert({
-    where: { id: imdb_id },
-    create: {
-      id: imdb_id,
-      name: data.name,
-      poster: data.poster,
-      description: data.description,
-      releaseInfo: data.releaseInfo,
-    },
-    update: {
-      name: data.name,
-      poster: data.poster,
-      description: data.description,
-    },
-  });
-
-  return upserted;
+  return meta;
 }
 
-async function get_imdb_id(film_name: string): Promise<Movie> {
+/** Gets many IMDB ID from films */
+async function get_imdb_id(film_name: string) {
   const id = await nameToImdb(film_name);
   if (!id) throw Error(`No IMDB ID found: ${film_name}`);
-  try {
-    const data = await get_tmdb_info(id);
-    if (!data) throw Error(`[${film_name}]: no data found`);
-    const poster = `https://images.metahub.space/poster/small/${data.id}/img`;
-    const name = data.name;
-    return {
-      id,
-      type: "movie",
-      name,
-      poster,
-      description: data.description,
-      releaseInfo: data.releaseInfo,
-    };
-  } catch (error) {
-    console.log("Error getting IMDB ID:", error);
-    return {
-      id: "",
-      type: "movie",
-      name: "",
-      poster: "",
-      description: "",
-    };
-  }
+  const data = await get_meta_info(id);
+  if (!data) throw Error(`[${film_name}]: no data found`);
+  return data;
 }
 
-async function get_imdb_ids(films: IFilm[]): Promise<Movie[]> {
+/** Gets an IMDB ID from a film */
+async function get_imdb_ids(films: IFilm[]) {
   return Promise.all(
     films.map((film) => `${film.slug} ${film.year}`).map(get_imdb_id)
   );
 }
 
-async function create_username_record(username: string, movies: Movie[]) {
+/** Gets Meta information for a single IMDB ID from Cinemeta */
+async function get_meta_single(imdb_id: `tt${number}` | string) {
+  const res = await fetch(
+    `https://v3-cinemeta.strem.io/meta/movie/${imdb_id}.json`
+  );
+  if (res.ok) {
+    const rv = await res.json();
+    return rv.meta;
+  } else {
+    throw Error(`[${imdb_id}] failed to get Cinemeta`);
+  }
+}
+
+/** Get Meta information for many IMDB IDs from Cinemeta */
+async function get_meta_many(imdb_ids: `tt${number}`[] | string[]) {
+  return Promise.all(imdb_ids.map(get_meta_single));
+}
+
+async function create_username_record(
+  username: string,
+  movies: { [key: string]: any }
+) {
   console.log(`Caching ${username} to database.`);
 
-  // create movies
-  const upsert = async (movie: Movie) => {
-    const data = await prisma.movie.upsert({
-      where: {
-        id: movie.id,
-      },
-      create: {
-        id: movie.id,
-        name: movie.name,
-        description: movie.description,
-        poster: movie.poster,
-      },
-      update: {
-        id: movie.id,
-        name: movie.name,
-        description: movie.description,
-        poster: movie.poster,
-      },
-    });
-
-    return data;
-  };
-  await Promise.all(movies.map(upsert));
-
-  // create user and relations
+  // create user
   const cached_user = await prisma.letterboxdUser.findUnique({
     where: { id: username },
   });
@@ -166,11 +98,11 @@ async function create_username_record(username: string, movies: Movie[]) {
     where: { id: username },
     create: {
       id: username,
-      movie_ids: JSON.stringify(movies.map((movie) => movie.id)),
+      movie_ids: JSON.stringify(movies.map((movie: any) => movie.imdb_id)),
     },
     update: {
       id: username,
-      movie_ids: JSON.stringify(movies.map((movie) => movie.id)),
+      movie_ids: JSON.stringify(movies.map((movie: any) => movie.imdb_id)),
     },
   });
 
@@ -184,31 +116,26 @@ async function get_cached_user(username: string) {
 
   if (!user) throw Error("no user found");
   if (is_old(user.updatedAt, config.cache_user_stale_time))
-    throw Error(`[${username}]: stale movie data`);
+    throw Error(`[${username}]: stale user data`);
 
   const parsed_movie_ids: string[] = JSON.parse(user.movie_ids);
-
-  const db_movies = await prisma.movie.findMany({
-    where: { id: { in: parsed_movie_ids } },
-  });
-
-  const movies = parsed_movie_ids.map((id) =>
-    db_movies.find((movie) => movie.id === id)
+  console.log(`[${username}]: got ${parsed_movie_ids.length} movie ids`);
+  const movie_info = await get_meta_many(parsed_movie_ids);
+  console.log(
+    `[${username}]: got metadata -> ${movie_info.map((m) => m.imdb_db)}`
   );
 
-  return { ...user, movies };
+  return { ...user, movies: movie_info };
 }
 
+/** fetch a Letterboxd user's watchlist */
 export async function watchlist_fetcher(
   username: string
-): Promise<{ source?: "fresh" | "cache"; metas: Movie[] }> {
+): Promise<{ source?: "fresh" | "cache"; metas: any }> {
   try {
     const cached_user_movies = await get_cached_user(username);
-    const purged_movies = cached_user_movies.movies.map((movie) => {
-      return { ...movie, type: "movie" };
-    }) as Movie[];
     console.log(`[${username}]: serving cached`);
-    return { source: "cache", metas: purged_movies };
+    return { source: "cache", metas: cached_user_movies.movies };
   } catch (error) {
     console.warn(`[${username}]: No user or old data, continuing..`);
   }
@@ -219,6 +146,7 @@ export async function watchlist_fetcher(
     const rawHtml = await (await fetch(Watchlist_URL(username))).text();
     const $ = cheerio(rawHtml);
 
+    // Get the film slugs from Letterboxd
     const filmSlugs = $(".poster")
       .map(function () {
         const slug = $(this).data().filmSlug as string;
@@ -226,7 +154,8 @@ export async function watchlist_fetcher(
       })
       .toArray();
 
-    const films = await Promise.all(
+    // Attempt to get the year of release from the detail page
+    const filmSlugs_and_years = await Promise.all(
       filmSlugs.map(async (slug) => {
         const filmPage = await (
           await fetch(`https://letterboxd.com/film/${slug}`)
@@ -240,9 +169,9 @@ export async function watchlist_fetcher(
     );
 
     // Only return stuff with an ID
-    const films_with_data = (await get_imdb_ids(films)).filter((movie) => {
-      return movie.id.length > 0;
-    });
+    const films_with_data = (await get_imdb_ids(filmSlugs_and_years)).map(
+      (film) => film.meta
+    );
 
     await create_username_record(username, films_with_data);
 
