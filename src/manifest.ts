d869fd278c0508a8a674ef85acd56345ffb8de63
Andy <a.simeon@gmail.com>
2024-03-03 20:48:51 +1100

    updated manifest to v1

--- src/manifest.ts
+++ src/manifest.ts
@@ -15,7 +15,7 @@ export type ManifestExpanded = Manifest & {
 
 const manifest: ManifestExpanded = {
   id: "github.megadrive.stremio.letterboxd",
-  version: "0.0.1",
+  version: "1.0.0",
   name: "Letterboxd Watchlist",
   logo: "https://stremio-letterboxd-watchlist.up.railway.app/logo.png",
   description: "Addon to add Letterboxd lists as catalogs",
