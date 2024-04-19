c026593ffc37dacafa19ae8b3fea55387e52cde3
Andy S <a.simeon@gmail.com>
2024-04-19 14:32:17 +1000

    popular lists woop

---
+++ web/src/popular.tsx
@@ -0,0 +1,54 @@
+function List({ id, name, url }: { id: string; name: string; url: string }) {
+  const installUrl = `stremio://${document.baseURI}/${id}/manifest.json`;
+  return (
+    <div className="grid gap-2 grid-cols-2">
+      <div className="text-right">
+        <a href={url} className="italic hover:underline">
+          {name}
+        </a>
+      </div>
+      <div className="grid grid-flow-col grid-cols-2 gap-1">
+        <button
+          className="border border-white rounded hover:bg-white hover:text-tailwind"
+          onClick={() => (window.location.href = installUrl)}
+        >
+          Install
+        </button>
+        <button
+          className="border border-transparent hover:border-white"
+          onClick={() =>
+            navigator.clipboard
+              .writeText(installUrl)
+              .then(() => alert("Copied!"))
+          }
+        >
+          Copy
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default function Popular() {
+  return (
+    <div>
+      <h2 className="text-center font-semibold text-xl mb-2">
+        Popular lists
+        <br />
+        <div className="text-sm">(first page only)</div>
+      </h2>
+      <div className="grid gap-1">
+        <List
+          id="_internal_|weekly"
+          name="Weekly"
+          url="https://letterboxd.com/films/popular/this/week/"
+        />
+        <List
+          id="_internal_|monthly"
+          name="Monthly"
+          url="https://letterboxd.com/films/popular/this/monthly/"
+        />
+      </div>
+    </div>
+  );
+}
