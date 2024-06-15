ae0a33a078445a846fbe3bb4f2063665a5dce9bf
Andy <a.simeon@gmail.com>
2024-06-15 22:12:01 +1000

    feat: selfhosting documentation.

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,13 @@
 import { cleanEnv, num, str, url } from "envalid";
 
 export const env = cleanEnv(process.env, {
+  /** Database URL, must relate to a Prisma-supported database. See /prisma.schema for more info. */
   DATABASE_URL: str(),
-  URL_DEV: url({ default: "http://localhost:3030" }),
-  URL_PRODUCTION: url({
-    default: "https://stremio-letterboxd-watchlist.up.railway.app",
-  }),
+  /** Server port to run on. If you deploy this to a service, they'll override this. */
   PORT: num({ default: 3030 }),
+  /**
+   * The maximum number of pages to fetch from a Letterboxd list. Ends up being around 980 movies for a user list, and 3500 for a "popular" list.
+   * Note that popular lists will only grab the first page.
+   * */
   ADDON_MAX_PAGES_FETCHED: num({ default: 35 }),
 });
