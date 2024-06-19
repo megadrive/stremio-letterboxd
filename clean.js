0290ab5711624ef6e4a8f1eddded14f3888716a7
Andrew Simeon <a.simeon@gmail.com>
2024-06-19 10:01:04 +1000

    feat: added web/.astro and a note about node_modules to clean.js

--- clean.js
+++ clean.js
@@ -1,12 +1,15 @@
 import { deleteAsync } from "del";
 
 const globs = [
-  ["./dist", "./web/dist"],
-  ["./static/**/*", "!./static", "!./static/*.{png,svg}"],
+	["./dist", "./web/dist"],
+	["./static/**/*", "!./static", "!./static/*.{png,svg}"],
+	["./web/.astro"],
 ];
 
 const promises = globs.map((glob) => deleteAsync(glob));
 
 Promise.all(promises).then(() => {
-  console.log("Working directories cleaned.");
+	console.log(
+		"Working directories cleaned, consider removoing node_modules from ./ and ./web",
+	);
 });
