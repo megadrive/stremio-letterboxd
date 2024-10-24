69e21d2bb2697baf10f5761fb48f712f9f29544d
Andy <a.simeon@gmail.com>
2024-10-24 20:06:18 +1100

    rename statis to public

--- clean.js
+++ clean.js
@@ -1,15 +1,15 @@
 import { deleteAsync } from "del";
 
 const globs = [
-	["./dist", "./web/dist"],
-	["./static/**/*", "!./static", "!./static/*.{png,svg}"],
-	["./web/.astro"],
+  ["./dist", "./web/dist"],
+  ["./public/**/*", "!./public", "!./public/*.{png,svg}"],
+  ["./web/.astro"],
 ];
 
 const promises = globs.map((glob) => deleteAsync(glob));
 
 Promise.all(promises).then(() => {
-	console.log(
-		"Working directories cleaned, consider removoing node_modules from ./ and ./web",
-	);
+  console.log(
+    "Working directories cleaned, consider removoing node_modules from ./ and ./web"
+  );
 });
