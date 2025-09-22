5b1343317ab94897467d9744f96721e469f1cb43
Andy <a.simeon@gmail.com>
2025-09-22 16:05:38 +1000

    fixed cache pathing

--- packages/server/src/lib/sqliteCache.ts
+++ packages/server/src/lib/sqliteCache.ts
@@ -4,7 +4,7 @@ import keyvSqlite from "@keyv/sqlite";
 
 export const createCache = <T>(namespace: string, ttl = 1000 * 60 * 60) => {
   return new keyv<T>({
-    store: new keyvSqlite({ uri: `sqlite://./.cache/${namespace}.sqlite` }),
+    store: new keyvSqlite({ uri: `sqlite://./${namespace}.db` }),
     ttl,
   });
 };
