0a527d177f18f56ba4510632ad44c9f096c6eb8a
Andy <a.simeon@gmail.com>
2025-10-01 14:47:07 +1000

    feat: db migration - remove cinemeta

---
+++ packages/database/prisma/migrations/20251001044608_remove_cinemeta/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Cinemeta` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "public"."Cinemeta";
